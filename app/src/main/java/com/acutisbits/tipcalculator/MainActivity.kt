15f2c239c312fe3c94c1ba134f75cf1fd15175d5
martinm27 <mihamac23@gmail.com>
2022-04-27 21:29:59 +0200

    Functionality implemented

--- app/src/main/java/com/acutisbits/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/acutisbits/tipcalculator/MainActivity.kt
@@ -1,7 +1,13 @@
 package com.acutisbits.tipcalculator
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.doAfterTextChanged
+
+private const val DEFAULT_EDIT_TEXT_VALUE = 0.0
+private const val HUNDRED_PERCENT_DENOMINATOR = 100
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,6 +15,43 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val tipAmount = findViewById<TextView>(R.id.tipAmount).apply {
+            text = DEFAULT_EDIT_TEXT_VALUE.toString()
+        }
+        val totalAmount = findViewById<TextView>(R.id.totalAmount).apply {
+            text = DEFAULT_EDIT_TEXT_VALUE.toString()
+        }
 
+        val billEditText = findViewById<EditText>(R.id.billAmountEditText)
+        val tipPercentageEditText = findViewById<EditText>(R.id.tipPercentageEditText).apply {
+            filters = arrayOf(PercentageNumberFilter())
+        }
+
+        billEditText.doAfterTextChanged {
+            val billAmount = it?.toString()?.toDoubleOrNull() ?: DEFAULT_EDIT_TEXT_VALUE
+            val tipPercentageAmount = tipPercentageEditText.text.toString().toDoubleOrNull() ?: DEFAULT_EDIT_TEXT_VALUE
+
+            calculateTipAndTotalAmount(billAmount, tipPercentageAmount, tipAmount, totalAmount)
+        }
+
+        tipPercentageEditText.doAfterTextChanged {
+            val tipPercentageAmount = it?.toString()?.toDoubleOrNull() ?: DEFAULT_EDIT_TEXT_VALUE
+            val billAmount = billEditText.text.toString().toDoubleOrNull() ?: DEFAULT_EDIT_TEXT_VALUE
+
+            calculateTipAndTotalAmount(billAmount, tipPercentageAmount, tipAmount, totalAmount)
+        }
+    }
+
+    private fun calculateTipAndTotalAmount(
+        billAmount: Double,
+        tipPercentageAmount: Double,
+        tipAmount: TextView,
+        totalAmount: TextView
+    ) {
+        val tipAmountCalculated = billAmount * tipPercentageAmount / HUNDRED_PERCENT_DENOMINATOR
+        tipAmount.text = tipAmountCalculated.toString()
+
+        val totalAmountCalculated = billAmount + tipAmountCalculated
+        totalAmount.text = totalAmountCalculated.toString()
     }
 }
---
+++ app/src/main/java/com/acutisbits/tipcalculator/PercentageNumberFilter.kt
@@ -0,0 +1,30 @@
+package com.acutisbits.tipcalculator
+
+import android.text.InputFilter
+import android.text.Spanned
+
+// Custom class to define min and max for the edit text
+class PercentageNumberFilter : InputFilter {
+
+    private val min = 0.0
+    private val max = 100.0
+
+    override fun filter(
+        source: CharSequence?,
+        p1: Int,
+        p2: Int,
+        destination: Spanned?,
+        p4: Int,
+        p5: Int
+    ): CharSequence? {
+        try {
+            val input = (destination.toString() + source.toString()).toDouble()
+            if (input in min..max) {
+                return null
+            }
+        } catch (e: NumberFormatException) {
+            e.printStackTrace()
+        }
+        return ""
+    }
+}
