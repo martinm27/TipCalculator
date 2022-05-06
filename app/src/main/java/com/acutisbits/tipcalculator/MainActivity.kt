c6dedae484de57a84e451d7978c59fc32ac014ed
Martin Mihalic <mihamac23@gmail.com>
2022-05-06 13:31:23 +0200

    Fixing working version a bit

--- app/src/main/java/com/acutisbits/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/acutisbits/tipcalculator/MainActivity.kt
@@ -8,34 +8,47 @@ import androidx.core.widget.doAfterTextChanged
 
 private const val DEFAULT_EDIT_TEXT_VALUE = 0.0
 private const val HUNDRED_PERCENT_DENOMINATOR = 100
+private const val DOLLAR_SIGN = "$"
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var tipAmount: TextView
+    private lateinit var totalAmount: TextView
+    private lateinit var billEditText: EditText
+    private lateinit var tipPercentageEditText: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val tipAmount = findViewById<TextView>(R.id.tipAmount).apply {
+        initialiseViews()
+        setTextChangeListeners()
+    }
+
+    private fun initialiseViews() {
+        tipAmount = findViewById<TextView>(R.id.tipAmount).apply {
             text = DEFAULT_EDIT_TEXT_VALUE.toString()
         }
-        val totalAmount = findViewById<TextView>(R.id.totalAmount).apply {
+        totalAmount = findViewById<TextView>(R.id.totalAmount).apply {
             text = DEFAULT_EDIT_TEXT_VALUE.toString()
         }
 
-        val billEditText = findViewById<EditText>(R.id.billAmountEditText)
-        val tipPercentageEditText = findViewById<EditText>(R.id.tipPercentageEditText).apply {
+        billEditText = findViewById(R.id.billAmountEditText)
+        tipPercentageEditText = findViewById<EditText>(R.id.tipPercentageEditText).apply {
             filters = arrayOf(PercentageNumberFilter())
         }
+    }
 
-        billEditText.doAfterTextChanged {
-            val billAmount = it?.toString()?.toDoubleOrNull() ?: DEFAULT_EDIT_TEXT_VALUE
+    private fun setTextChangeListeners() {
+        billEditText.doAfterTextChanged { editableText ->
+            val billAmount = editableText?.toString()?.toDoubleOrNull() ?: DEFAULT_EDIT_TEXT_VALUE
             val tipPercentageAmount = tipPercentageEditText.text.toString().toDoubleOrNull() ?: DEFAULT_EDIT_TEXT_VALUE
 
             calculateTipAndTotalAmount(billAmount, tipPercentageAmount, tipAmount, totalAmount)
         }
 
-        tipPercentageEditText.doAfterTextChanged {
-            val tipPercentageAmount = it?.toString()?.toDoubleOrNull() ?: DEFAULT_EDIT_TEXT_VALUE
+        tipPercentageEditText.doAfterTextChanged { editableText ->
+            val tipPercentageAmount = editableText?.toString()?.toDoubleOrNull() ?: DEFAULT_EDIT_TEXT_VALUE
             val billAmount = billEditText.text.toString().toDoubleOrNull() ?: DEFAULT_EDIT_TEXT_VALUE
 
             calculateTipAndTotalAmount(billAmount, tipPercentageAmount, tipAmount, totalAmount)
@@ -46,12 +59,15 @@ class MainActivity : AppCompatActivity() {
         billAmount: Double,
         tipPercentageAmount: Double,
         tipAmount: TextView,
-        totalAmount: TextView
+        totalAmount: TextView,
+        currency: String = DOLLAR_SIGN
     ) {
         val tipAmountCalculated = billAmount * tipPercentageAmount / HUNDRED_PERCENT_DENOMINATOR
-        tipAmount.text = tipAmountCalculated.toString()
+        val tipAmountWithCurrency = "$currency$tipAmountCalculated"
+        tipAmount.text = tipAmountWithCurrency
 
         val totalAmountCalculated = billAmount + tipAmountCalculated
-        totalAmount.text = totalAmountCalculated.toString()
+        val totalAmountWithCurrency = "$currency$totalAmountCalculated"
+        totalAmount.text = totalAmountWithCurrency
     }
 }
